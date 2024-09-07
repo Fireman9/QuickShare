7ef4e6f94f57e33828a8e4fc41f8700120d2c185
Fireman9 <nazariystr@gmail.com>
2024-09-07 15:07:08 +0300

    added: -set severity lvl - set console write - new log format

--- src/common/logger.hpp
+++ src/common/logger.hpp
@@ -1,11 +1,13 @@
 #ifndef __LOGGER_HPP__
 #define __LOGGER_HPP__
 
+#include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/sources/severity_logger.hpp>
 #include <boost/log/support/date_time.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/file.hpp>
 
 #define LOG_TRACE   BOOST_LOG_TRIVIAL(trace)
@@ -15,22 +17,46 @@
 #define LOG_ERROR   BOOST_LOG_TRIVIAL(error)
 #define LOG_FATAL   BOOST_LOG_TRIVIAL(fatal)
 
-/// @brief Initializes the logging system. File sink, log file name, logging
-/// format, severity level
-inline void init_logging()
+namespace logger = boost::log;
+namespace trivial = boost::log::trivial;
+namespace expr = boost::log::expressions;
+namespace attrs = boost::log::attributes;
+namespace keywords = boost::log::keywords;
+
+/// @brief Initializes the logging system with configurable severity level and
+/// output destinations.
+/// @param min_severity The minimum severity level for logging.
+/// @param log_to_file Whether to log to a file.
+/// @param log_to_console Whether to log to the console.
+inline void init_logging(trivial::severity_level min_severity = trivial::info,
+                         bool log_to_file = true, bool log_to_console = true)
 {
-    boost::log::register_simple_formatter_factory<
-        boost::log::trivial::severity_level, char>("Severity");
+    logger::register_simple_formatter_factory<trivial::severity_level, char>(
+        "Severity");
 
-    boost::log::add_file_log(
-        boost::log::keywords::file_name = "Quickshare_%N.log",
-        boost::log::keywords::format =
-            "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%");
+    auto log_formatter =
+        expr::stream << "["
+                     << expr::format_date_time<boost::posix_time::ptime>(
+                            "TimeStamp", "%Y-%m-%d %H:%M:%S")
+                     << "] ["
+                     << expr::attr<attrs::current_thread_id::value_type>(
+                            "ThreadID")
+                     << "] [" << trivial::severity << "] " << expr::smessage;
 
-    boost::log::core::get()->set_filter(boost::log::trivial::severity >=
-                                        boost::log::trivial::info);
+    if (log_to_file)
+    {
+        logger::add_file_log(keywords::file_name = "Quickshare_%N.log",
+                          keywords::format = log_formatter);
+    }
 
-    boost::log::add_common_attributes();
+    if (log_to_console)
+    {
+        logger::add_console_log(std::cout, keywords::format = log_formatter);
+    }
+
+    logger::core::get()->set_filter(trivial::severity >= min_severity);
+
+    logger::add_common_attributes();
 }
 
 #endif // __LOGGER_HPP__
